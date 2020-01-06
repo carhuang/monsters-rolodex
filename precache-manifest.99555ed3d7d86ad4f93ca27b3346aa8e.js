9716ef429c1b6149d1e5e08b2540252a87bf3ba9
Carly Huang <c85615@gmail.com>
2020-01-05 21:07:03 -0800

    Updates

---
+++ precache-manifest.99555ed3d7d86ad4f93ca27b3346aa8e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aac643c63ae4372b36441b168efe98db",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "4d8d8c2ae9e476df014a",
+    "url": "/monsters-rolodex/static/css/main.cea322e3.chunk.css"
+  },
+  {
+    "revision": "5f64445c3b396716b777",
+    "url": "/monsters-rolodex/static/js/2.7e82c9cb.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.7e82c9cb.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4d8d8c2ae9e476df014a",
+    "url": "/monsters-rolodex/static/js/main.d7011982.chunk.js"
+  },
+  {
+    "revision": "8e0631e8fb00ca5a6969",
+    "url": "/monsters-rolodex/static/js/runtime-main.41315a6c.js"
+  }
+]);
